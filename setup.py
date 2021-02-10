98bab7dc648005e6691673c82962a6231fa0e724
JOhn Huang <johnhuang865@gmail.com>
2021-02-10 12:18:26 -0600

    increment version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ install_requires = [
 #
 setup(
       name='votesim',
-      version='1.0',
+      version='1.0.1',
       packages=find_packages(),
       install_requires=install_requires,
       zip_safe=False,
